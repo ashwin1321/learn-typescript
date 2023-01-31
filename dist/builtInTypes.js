7bd4c51f9357530ce8b77a98e4cf06abdac2ea7a
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-01-31 16:05:53 +0545

    tuples and enums

--- dist/builtInTypes.js
+++ dist/builtInTypes.js
@@ -8,3 +8,8 @@ anyType = "Hello";
 function render(document) {
     console.log(document);
 }
+let numbers = [1, 2, 3, 4, 5];
+let anyArray = [1, "Hello", true];
+let tuple = ["Hello", 10];
+let backgroundColor = "r";
+console.log(backgroundColor);
--- src/builtInTypes.ts
+++ src/builtInTypes.ts
@@ -9,3 +9,20 @@ anyType = "Hello";
 function render(document: any) {
   console.log(document);
 }
+
+// Array
+let numbers: number[] = [1, 2, 3, 4, 5];
+let anyArray: any[] = [1, "Hello", true]; // not recommended
+
+// Tuple
+let tuple: [string, number] = ["Hello", 10]; // each element has its own type and order matters. is of fixed length
+
+// Enum
+const enum Color { // list of related constants
+  Red = "r",
+  Green = "g",
+  Blue = "b",
+}
+
+let backgroundColor = Color.Red;
+console.log(backgroundColor);
