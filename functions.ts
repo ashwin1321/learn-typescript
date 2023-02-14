1a5ac7ef4ce248c43ff78e91c17111705640fc8b
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-14 12:43:09 +0545

    return types in function

--- functions.ts
+++ functions.ts
@@ -1,5 +1,8 @@
-function addTwo(num: number) {
+function addTwo(num: number): number {
+  // return type is number
+
   //   num.toUppercase();
+  //   return "hello";
   return num + 2;
 }
 
@@ -14,5 +17,34 @@ function getUpper(
 
 addTwo(5);
 getUpper("hello", "abc", 1);
+
+// ---> it will give error <---
+// function getaval(val: string): boolean {
+//   if (typeof val === "string") {
+//     return true;
+//   } else {
+//     return "hello";
+//   }
+// }
+
+const getAns = (s: string): string => {
+  return "";
+};
+
+const color = ["red", "green", "blue"];
+// const color = [1, 2, 3];
+color.map((color): string => {
+  return `color is ${color}`;
+});
+
+function consoleError(errmsg: string): void {
+  // return type is void
+  console.log(errmsg);
+}
+
+function handleError(errmsg: string): never {
+  // some function will never return anything
+  throw new Error(errmsg);
+}
 
 export {};
