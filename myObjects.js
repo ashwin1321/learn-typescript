8aab2c84b20fceb377d05e2e938f1b4f81aabf78
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-14 19:28:04 +0545

    bad behavious of objectS

--- myObjects.js
+++ myObjects.js
@@ -1,9 +1,20 @@
+"use strict";
+exports.__esModule = true;
 var User = {
-    name: "Ashwin",
-    email: "ashwin@gmail.com",
-    isActive: true
+  name: "Ashwin",
+  email: "ashwin@gmail.com",
+  isActive: true,
 };
+// objects in normal javascripts
 function createUser(_a) {
-    var string = _a.name, boolean = _a.isPaid;
+  var string = _a.name,
+    boolean = _a.isPaid;
 }
-createUser();
+var newUser = { name: "Ashwin", isPaid: true, email: "a@gmail.com" };
+// createUser({ name: "Ashwin", isPaid: true });
+createUser(newUser); // not a good way
+// objects in typescript in a function
+function createCourse() {
+  // (): {This is the return type} {}
+  return { name: "Ashwin", price: 2 };
+}
