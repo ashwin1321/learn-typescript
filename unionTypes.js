07b729ebcbaa4b34ba937ac4a15f96347219b246
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-02-15 12:05:00 +0545

    union types

---
+++ unionTypes.js
@@ -0,0 +1,32 @@
+"use strict";
+// union types in typescript are used to combine two or more types into one type and it is used to represent a value that can be one of several types.
+exports.__esModule = true;
+var score = 33; // score is a number or string
+score = 44;
+score = "44";
+var ashwin = {
+    // ashwin is a User or Admin
+    name: "ashwin",
+    id: 1
+};
+ashwin = { username: "ashwin123", id: 22 }; // ashwin is a Admin
+// function getDbId(id: number | string) {
+//   // making some API call to get the id
+//   console.log(`DB id is: ${id}`);
+// }
+getDbId(2);
+getDbId("2");
+function getDbId(id) {
+    // this acts as both number and string type
+    if (typeof id === "string") {
+        id.toLowerCase(); // no error
+    }
+    if (typeof id === "number") {
+        id.toFixed(); // no error
+    }
+    //   id.toLowerCase(); // error
+}
+// arrays of union types
+var data = [1, 2, 3, 4, 5]; // array of numbers
+var data1 = ["four", "string"]; // array of strings
+var data2 = [1, 2, 3, "four", "string"]; // array of numbers and strings
---
+++ unionTypes.ts
@@ -0,0 +1,49 @@
+// union types in typescript are used to combine two or more types into one type and it is used to represent a value that can be one of several types.
+
+let score: number | string = 33; // score is a number or string
+score = 44;
+score = "44";
+
+type User = {
+  name: string;
+  id: number;
+};
+
+type Admin = {
+  username: string;
+  id: number;
+};
+
+let ashwin: User | Admin = {
+  // ashwin is a User or Admin
+  name: "ashwin",
+  id: 1,
+};
+ashwin = { username: "ashwin123", id: 22 }; // ashwin is a Admin
+
+// function getDbId(id: number | string) {
+//   // making some API call to get the id
+//   console.log(`DB id is: ${id}`);
+// }
+getDbId(2);
+getDbId("2");
+
+function getDbId(id: number | string) {
+  // this acts as both number and string type
+  if (typeof id === "string") {
+    id.toLowerCase(); // no error
+  }
+
+  if (typeof id === "number") {
+    id.toFixed(); // no error
+  }
+  //   id.toLowerCase(); // error
+}
+
+// arrays of union types
+const data: number[] = [1, 2, 3, 4, 5]; // array of numbers
+const data1: string[] = ["four", "string"]; // array of strings
+const data2: (number | string)[] = [1, 2, 3, "four", "string"]; // array of numbers and strings
+// const dataa: number[] | string[] = [1, 2, 3, "4", "5"];  // error because it is not a union type
+
+export {};
